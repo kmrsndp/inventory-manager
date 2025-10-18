79e1b9c3c2c208070c10ae49b7b0fa0c08e12f23
kmrsndp <kmr.sndp@gmail.com>
2025-10-18 15:55:48 +0530

    feat: removed unnecessary changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-    ],
-  },
+  output: 'export',
   /* config options here */
 };
 
